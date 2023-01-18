623f5a4576de1a3316709e602ce5a9fbb0703cc4
jeonghs <hsjung201558@gmail.com>
2023-01-18 17:25:38 +0900

    Feed model migrate

--- content/models.py
+++ content/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Feed(models.Model):
+    content = models.TextField()        # 글 내용
+    image = models.TextField()          # 피드 이미지
+    profile_image = models.TextField()  # 프로필 이미지
+    user_id = models.TextField()        # 글 작성자
+    like_count = models.IntegerField()  # 좋아요 수
