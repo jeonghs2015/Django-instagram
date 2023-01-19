124de0672b0c779fc366ae9309dd7b1d65cba512
jeonghs <hsjung201558@gmail.com>
2023-01-19 14:24:15 +0900

    url mapping 수정, views에서 데이터 불러오기

--- content/views.py
+++ content/views.py
@@ -4,4 +4,6 @@ from .models import Feed
 
 class Main(APIView):
     def get(self, request):
+        feed_list = Feed.objects.all()  # select * from content_feed
+        print(feed_list)
         return render(request, "jinstagram/main.html")
--- jinstagram/urls.py
+++ jinstagram/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from .views import Sub
+from content.views import Main
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('main/', Sub.as_view())
+    path('main/', Main.as_view())
 ]
