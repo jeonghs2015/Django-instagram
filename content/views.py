00fa16f0801bd76c9a08bb6a820c59e71ada73d3
jeonghs <hsjung201558@gmail.com>
2023-01-20 10:44:13 +0900

    views 페이지 수정

--- content/views.py
+++ content/views.py
@@ -6,3 +6,4 @@ class Main(APIView):
     def get(self, request):
         feed_list = Feed.objects.all().order_by("-id")  # select * from content_feed
         return render(request, "jinstagram/main.html", context=dict(feed_list=feed_list))
+
