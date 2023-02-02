d3808bedcfb46ddf7c2fd23dcc194cb55a635b86
jeonghs <hsjung201558@gmail.com>
2023-02-02 11:31:13 +0900

    urlpatterns, static 관련 내용 추가

--- jinstagram/urls.py
+++ jinstagram/urls.py
@@ -17,9 +17,13 @@ from django.contrib import admin
 from django.urls import path
 from .views import Sub
 from content.views import Main, UploadFeed
+import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('main/', Main.as_view()),
     path('content/upload', UploadFeed.as_view())
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
